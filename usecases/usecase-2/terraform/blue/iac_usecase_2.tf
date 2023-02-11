e1c1a2b1c6ceae790a91effe4c563ce49110c454
Daniyah <101661796+daniyahsiddiqui@users.noreply.github.com>
2023-02-11 17:49:18 -0600

    Update iac_usecase_2.tf

--- usecases/usecase-2/terraform/blue/iac_usecase_2.tf
+++ usecases/usecase-2/terraform/blue/iac_usecase_2.tf
@@ -240,7 +240,7 @@ resource "aws_instance" "app_server_usecase2_blue" {
   key_name  = var.KEYNAME
   instance_type = var.EC2_TYPE
   subnet_id = var.SUBNET
-  iam_instance_profile = aws_iam_instance_profile.rm_iam_profile.name  
+  iam_instance_profile = aws_iam_instance_profile.rm_iam_profile_blue.name  
   associate_public_ip_address = true
   vpc_security_group_ids = [
 	aws_security_group.basic_http.id,
