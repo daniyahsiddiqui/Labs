6e11ee2632abacc173bd7db18aef5bb05c823374
Daniyah Siddiqui <daniyah@gmail.com>
2023-02-11 17:35:07 -0600

    Changed setup.py

--- usecases/usecase-2/setup.py
+++ usecases/usecase-2/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='my_application',
-    version='1.0.0',
+    version='1.0.1',
     packages=['usecases/usecase-2/my_application'],
     include_package_data=True,
     install_requires=[
